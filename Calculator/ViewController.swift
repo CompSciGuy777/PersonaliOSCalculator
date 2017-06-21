18b25c6702b74081a03d3cf9c7f882077242b346
Chris Davis <christopher.davis777@yahoo.com>
2017-06-21 22:11:13 +0900

    Adding a Square Root button and cleaning up the code.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    @IBOutlet weak var display: UILabel?
+    
+    @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTyping = false
     
@@ -29,23 +29,35 @@ class ViewController: UIViewController {
         let digit = sender.currentTitle!
         
         if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display!.text!
-            display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         } else{
-            display!.text = digit
+            display.text = digit
             userIsInTheMiddleOfTyping = true
         }
     }
-
+    var displayValue: Double {
+        get{
+            return Double(display.text!)!
+        }
+        
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    
     @IBAction func preformOperation(_ sender: UIButton) {
         userIsInTheMiddleOfTyping = false
         if let mathimaticalSymbol = sender.currentTitle {
             switch mathimaticalSymbol {
             case "π":
-                display!.text = String(Double.pi)
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
             default:
                 break
+            }
         }
     }
-}
 }
